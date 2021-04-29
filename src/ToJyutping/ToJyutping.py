1f75e4625a3107b5bd551a491295a619d4911b4d
graphemecluster <graphemecluster@gmail.com>
2021-04-30 05:57:08 +0800

    Update

--- src/ToJyutping/ToJyutping.py
+++ src/ToJyutping/ToJyutping.py
@@ -19,7 +19,7 @@ def get_jyutping_list(s):
 			s = s[1:]
 		else:
 			n = len(p.key)
-			l += zip(s[:n], p.value.split(' '))
+			l += zip(s[:n], p.value.split(' ', n - 1))
 			s = s[n:]
 	return l
 
@@ -28,6 +28,13 @@ def get_jyutping(s):
 	for k, v in get_jyutping_list(s):
 		l += k + ('(%s)' % v if v else '')
 	return l
+
+def get_jyutping_text(s):
+	l = []
+	for k, v in get_jyutping_list(s):
+		if v:
+			l += [v]
+	return ' '.join(l)
 
 def get_ipa_list(s):
 	l = []
@@ -41,6 +48,13 @@ def get_ipa(s):
 		l += k + ('[%s]' % jyutping2ipa(v) if v else '')
 	return l
 
+def get_ipa_text(s):
+	l = []
+	for k, v in get_jyutping_list(s):
+		if v:
+			l += [jyutping2ipa(v)]
+	return '.'.join(l)
+
 initial = { 'b': 'p', 'p': 'pʰ', 'm': 'm', 'f': 'f', 'd': 't', 't': 'tʰ', 'n': 'n', 'l': 'l', 'g': 'k', 'k': 'kʰ',
 			'ng': 'ŋ', 'gw': 'kʷ', 'kw': 'kʷʰ', 'w': 'w', 'h': 'h', 'z': 't͡s', 'c': 't͡sʰ', 's': 's', 'j': 'j' }
 nucleus = { 'aa': 'aː', 'a': 'ɐ', 'e': 'ɛː', 'i': 'iː', 'o': 'ɔː', 'u': 'uː', 'oe': 'œː', 'eo': 'ɵ', 'yu': 'yː' }
@@ -49,9 +63,13 @@ terminal = { 'i': 'i̯', 'u': 'u̯', 'm': 'm', 'n': 'n', 'ng': 'ŋ', 'p': 'p̚',
 tone = { '1': '˥', '2': '˧˥', '3': '˧', '4': '˨˩', '5': '˩˧', '6': '˨' }
 
 def jyutping2ipa(s):
-	match = re.match('^((?:[gk]w?|ng|[bpmfdtnlhwzcsj])?)((?:aa?|oe|eo|yu|[eiou])?)((?:ng|[iumnptk])?)((?:[1-6])?)$', s)
-	lead, vowel, final, number = match.groups()
-	group = vowel + final
-	return (lead and initial[lead]) + (
-		unit[group] if group in unit else (vowel and nucleus[vowel]) + (final and terminal[final])
-	) + (tone and tone[number])
+	l = []
+	for t in re.split("[\s'.]+", s):
+		match = re.match('^((?:[gk]w?|ng|[bpmfdtnlhwzcsj])?)((?:aa?|oe|eo|yu|[eiou])?)((?:ng|[iumnptk])?)((?:[1-6])?)$', t)
+		lead, vowel, final, number = match.groups()
+		group = vowel + final
+		l += [(lead and initial[lead])
+			+ (unit[group] if group in unit else (vowel and nucleus[vowel]) + (final and terminal[final]))
+			+ (tone and tone[number])
+		]
+	return '.'.join(l)
